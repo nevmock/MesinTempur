7eaffedbd2d86fc4841f7bb42368654bcbe26cbf
optimus9021 <ismaianugrah@gmail.com>
2025-03-25 10:45:06 +0700

    adding new route to instagram rapid

--- src/domains/Instagram/RapidAPI/instagram-rapid-controller.ts
+++ src/domains/Instagram/RapidAPI/instagram-rapid-controller.ts
@@ -50,6 +50,29 @@ class InstagramRapidController {
          });
       }
    }
+
+   public getUserAndPostData = async (
+      req: Request,
+      res: Response,
+   ): Promise<Response> => {
+      console.info(req.body.kategori)
+      try {
+         const result = await this.instagramRapidServices.getUserAndPostData(
+            req.body.kategori
+         )
+         return res.status(200).json({
+            code: 200,
+            status: 'OK',
+            data: result,
+         });
+      } catch (error) {
+         return res.status(500).json({
+            code: 500,
+            status: 'ERROR',
+            data: error,
+         });
+      }
+   }
 }
 
 export default new InstagramRapidController();
--- src/domains/Instagram/RapidAPI/instagram-rapid-services.ts
+++ src/domains/Instagram/RapidAPI/instagram-rapid-services.ts
@@ -28,6 +28,22 @@ class InstagramRapidServices {
          await this.instagramRapidRepository.getDataPost(user);
       }
    }
+
+   public async getUserAndPostData(kategori: string | null) {
+      const result = await this.instagramRapidRepository.getUserDB(kategori);
+
+      const usernames = result.map((item: any) => item.username);
+
+      const semuaDataGabungan: any[] = [];
+
+      for (const user of usernames) {
+         console.info("Getting data for:", user);
+         const dataGabunganPerUser = await this.instagramRapidRepository.getUserAndPostData(user);
+         semuaDataGabungan.push(...dataGabunganPerUser); // gabungkan semua post ke array utama
+      }
+
+      return semuaDataGabungan;
+   }
 }
 
 export default InstagramRapidServices;
